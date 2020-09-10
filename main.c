2581d27c66dc9f5ea56b5e666f9c488cbd796f81
shj312 <sshj312@naver.com>
2020-09-10 22:24:44 +0900

    hello

--- main.c
+++ main.c
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	prinf("Hello Sookmyunng Electroics!\n");
+	printf("Hello, Sookmyung Electroncs!\n");
 	return 0;
 }
