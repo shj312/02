a0e6c111bd4402904c67f1a4a49f4d12084aa51f
shj312 <sshj312@naver.com>
2020-09-11 10:42:58 +0900

    re-submission

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello, Sookmyung Electroncs!\n");
+	//printf("Hello, Sookmyung Electroncs!\n");
+	//실습 4번부터 하시면 됩니다.
+	
 	return 0;
 }
